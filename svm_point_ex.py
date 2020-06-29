8e04d18947de4ceb73f9df4ec92f5151c769e370
flippedcoder <milecia.mcgregor@aol.com>
2020-06-28 21:41:36 -0500

    cleaned up example code

--- svm_point_ex.py
+++ svm_point_ex.py
@@ -3,10 +3,11 @@ import numpy as np
 from sklearn import datasets
 from sklearn import svm
 
-def plot_svc_decision_function(model, ax=None, plot_support=True):
-    """Plot the decision function for a 2D SVC"""
+# Plot the decision boundary for a non-linear SVM problem
+def plot_decision_boundary(model, ax=None):
     if ax is None:
         ax = plt.gca()
+        
     xlim = ax.get_xlim()
     ylim = ax.get_ylim()
     
@@ -14,11 +15,15 @@ def plot_svc_decision_function(model, ax=None, plot_support=True):
     x = np.linspace(xlim[0], xlim[1], 30)
     y = np.linspace(ylim[0], ylim[1], 30)
     Y, X = np.meshgrid(y, x)
+
+	# shape data
     xy = np.vstack([X.ravel(), Y.ravel()]).T
+
+    # get the decision boundary based on the model
     P = model.decision_function(xy).reshape(X.shape)
     
-    # plot decision boundary and margins
-    ax.contour(X, Y, P, colors='k',
+    # plot decision boundary
+    ax.contour(X, Y, P,
                levels=[0], alpha=0.5,
                linestyles=['-'])
 
@@ -27,36 +32,43 @@ X = np.array([1, 5, 1.5, 8, 1, 9, 7, 8.7, 2.3, 5.5, 7.7, 6.1])
 y = np.array([2, 8, 1.8, 8, 0.6, 11, 10, 9.4, 4, 3, 8.8, 7.5])
 
 # show unclassified data
-# plt.scatter(X, y)
-# plt.show()
+plt.scatter(X, y)
+plt.show()
 
 # shaping data for training the model
 training_X = np.vstack((X, y)).T
 training_y = [0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1]
 
-# define the algorithm for the model
+# define the model
 clf = svm.SVC(kernel='linear', C=1.0)
 
 # train the model
 clf.fit(training_X, training_y)
 
+# get the weight values for the linear equation from the trained SVM model
 w = clf.coef_[0]
 
+# get the y-offset for the linear equation
 a = -w[0] / w[1]
 
-XX = np.linspace(0, 12)
+# make the x-axis space for the data points
+XX = np.linspace(0, 13)
+
+# get the y-values to plot the decision boundary
 yy = a * XX - clf.intercept_[0] / w[1]
 
-# h0 = plt.plot(XX, yy, 'k-', label='non weighted division')
+# plot the decision boundary
+plt.plot(XX, yy, 'k-')
 
-# plt.scatter(training_X[:, 0], training_X[:, 1], c=training_y)
-# plt.legend()
-# plt.show()
+# show the plot visually
+plt.scatter(training_X[:, 0], training_X[:, 1], c=training_y)
+plt.legend()
+plt.show()
 
 # non-linear data
 circle_X, circle_y = datasets.make_circles(n_samples=300, noise=0.05)
 
-# show unclassified non-linear data
+# show raw non-linear data
 plt.scatter(circle_X[:, 0], circle_X[:, 1], c=circle_y, marker='.')
 plt.show()
 
@@ -66,7 +78,8 @@ nonlinear_clf = svm.SVC(kernel='rbf', C=1.0)
 # training non-linear model
 nonlinear_clf.fit(circle_X, circle_y)
 
-plt.scatter(circle_X[:, 0], circle_X[:, 1], c=circle_y, s=50, cmap='autumn')
-plot_svc_decision_function(nonlinear_clf)
-plt.scatter(nonlinear_clf.support_vectors_[:, 0], nonlinear_clf.support_vectors_[:, 1], s=200, lw=1, facecolors='none')
+# plot data and decision boundary
+plt.scatter(circle_X[:, 0], circle_X[:, 1], c=circle_y, s=50)
+plot_decision_boundary(nonlinear_clf)
+plt.scatter(nonlinear_clf.support_vectors_[:, 0], nonlinear_clf.support_vectors_[:, 1], s=50, lw=1, facecolors='none')
 plt.show()
